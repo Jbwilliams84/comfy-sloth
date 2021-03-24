4e71a0252a63c360710ac29812b33ed593a4b274
Jbwilliams84 <jbwilliams484@gmail.com>
2021-03-24 13:50:20 -0500

    updated product filters

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,4 +5,12 @@ export const formatPrice = (number) => {
 	}).format(number / 100);
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+	// iterating over all products and grabbing the category and only listing them once
+	let unique = data.map((item) => item[type]);
+	if (type === 'colors') {
+		unique = unique.flat();
+	}
+	return ['all', ...new Set(unique)];
+	console.log(unique);
+};
